4fbeb3f14d331d19013edd859fa11aeb9666243e
pthpv95 <thehienpv@gmail.com>
2016-12-08 00:49:00 +0700

    readonly theory

--- cSharp/Program.cs
+++ cSharp/Program.cs
@@ -10,12 +10,18 @@ namespace cSharp
     {
         public static void Main(string[] args)
         {
+            //this is singleton
             Student student = Student.GetStudentInstance();
             student.Name = "One";
             Console.WriteLine(student.Name);
             student.Name = "Two";
             Console.WriteLine(student.Name);
-            //this is singleton
+           
+
+            //readonly can change value during runtime
+            Variable va = new Variable();
+            Variable va1 = new Variable("Changed");
+            
         }
 
     }
@@ -36,5 +42,23 @@ namespace cSharp
             return student;
         }
        
+    }
+
+    public class Variable
+    {
+        readonly string re = "Hien";
+
+
+        public Variable()
+        {
+            re = "This readony string has been changed during runtime";
+            Console.WriteLine(re);
+        }
+
+        public Variable(string variable)
+        {
+            re = variable + "This readony string has been changed during runtime";
+            Console.WriteLine(re);
+        }
     }
 }
